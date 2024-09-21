0987838ef8f5f3ad900cef802074119504d4ac1c
JayLee <sk8terbo2@gmail.com>
2024-09-21 22:07:04 +0900

    Update on 2024-09-21

---
+++ src/git_push.py
@@ -0,0 +1,20 @@
+from utils.config import PathDictionary
+from utils.utils import batch_manager, get_task_id
+from git import Repo
+from loguru import logger
+from datetime import datetime
+
+def git_push(date_id: str):
+    try:
+        repo = Repo(PathDictionary.root)
+        repo.git.add(".")
+        repo.index.commit(message=f"Update on {date_id}")
+        origin = repo.remote(name="origin")
+        origin.push()
+    except Exception as e:
+        logger.error(e)
+
+if __name__ == "__main__":
+    date_id = datetime.now().strftime("%Y-%m-%d")
+    git_push(date_id)
+    # batch_manager(task_id=get_task_id(__file__), key=date_id, func=git_push, date_id=date_id)
