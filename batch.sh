124ab11757873a7707e4b6fc5c4bb26c88b98fd0
JayLee <sk8terbo2@gmail.com>
2024-10-09 12:00:51 +0900

    Update on 2024-10-09

--- batch.sh
+++ batch.sh
@@ -1,17 +1,17 @@
-/Users/kakao/.base/bin/python /Users/kakao/Desktop/notebook/study/apt_trade/src/git_pull.py;
-/Users/kakao/.base/bin/python /Users/kakao/Desktop/notebook/study/apt_trade/src/apt_trade.py;
-/Users/kakao/.base/bin/python /Users/kakao/Desktop/notebook/study/apt_trade/src/bunyang_trade.py;
-/Users/kakao/.base/bin/python /Users/kakao/Desktop/notebook/study/apt_trade/src/sales.py;
-/Users/kakao/.base/bin/python /Users/kakao/Desktop/notebook/study/apt_trade/src/analysis.py;
-/Users/kakao/.base/bin/python /Users/kakao/Desktop/notebook/study/apt_trade/src/notifier.py;
-/Users/kakao/.base/bin/python /Users/kakao/Desktop/notebook/study/apt_trade/src/git_push.py;
+#/Users/kakao/.base/bin/python /Users/kakao/Desktop/notebook/study/apt_trade/src/git_pull.py;
+#/Users/kakao/.base/bin/python /Users/kakao/Desktop/notebook/study/apt_trade/src/apt_trade.py;
+#/Users/kakao/.base/bin/python /Users/kakao/Desktop/notebook/study/apt_trade/src/bunyang_trade.py;
+#/Users/kakao/.base/bin/python /Users/kakao/Desktop/notebook/study/apt_trade/src/sales.py;
+#/Users/kakao/.base/bin/python /Users/kakao/Desktop/notebook/study/apt_trade/src/analysis.py;
+#/Users/kakao/.base/bin/python /Users/kakao/Desktop/notebook/study/apt_trade/src/notifier.py;
+#/Users/kakao/.base/bin/python /Users/kakao/Desktop/notebook/study/apt_trade/src/git_push.py;
 
 # local
-#/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/git_pull.py;
-#/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/apt_trade.py;
-#/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/bunyang_trade.py;
-#/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/sales.py;
-#/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/analysis.py;
-#/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/notifier.py;
-#/Users/user/python_env/.base/bin/python3.10 /Users/user/Desktop/study/apt_trade/src/git_push.py;
+/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/git_pull.py;
+/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/apt_trade.py;
+/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/bunyang_trade.py;
+/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/sales.py;
+/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/analysis.py;
+/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/notifier.py;
+/Users/user/python_env/.base/bin/python3.10 /Users/user/Desktop/study/apt_trade/src/git_push.py;
 
