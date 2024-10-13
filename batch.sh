2edb4c399ba6d84bf9a10c0abdd68096c2e52dc3
JayLee <sk8terbo2@gmail.com>
2024-10-13 19:55:58 +0900

    fix: font error

--- batch.sh
+++ batch.sh
@@ -7,11 +7,11 @@
 #/Users/kakao/.base/bin/python /Users/kakao/Desktop/notebook/study/apt_trade/src/git_push.py;
 
 # local
-#/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/git_pull.py;
-#/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/apt_trade.py;
-#/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/bunyang_trade.py;
-#/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/sales.py;
-/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/analysis.py --mode=test --nonblock;
-#/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/notifier.py;
-#/Users/user/python_env/.base/bin/python3.10 /Users/user/Desktop/study/apt_trade/src/git_push.py;
+/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/git_pull.py;
+/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/apt_trade.py;
+/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/bunyang_trade.py;
+/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/sales.py;
+/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/analysis.py;
+/Users/user/python_env/.base/bin/python3.10  /Users/user/Desktop/study/apt_trade/src/notifier.py;
+/Users/user/python_env/.base/bin/python3.10 /Users/user/Desktop/study/apt_trade/src/git_push.py;
 
