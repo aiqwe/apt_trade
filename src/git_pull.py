4c2dc45676c3b4dfae9369377f205ee610adab36
JayLee <sk8terbo2@gmail.com>
2024-10-27 22:01:27 +0900

    chore: fix git

--- src/git_pull.py
+++ src/git_pull.py
@@ -1,5 +1,7 @@
 from utils import PathConfig
 from utils import BatchManager, get_task_id
+
+from argparse import ArgumentParser
 from git import Repo
 from loguru import logger
 from datetime import datetime
@@ -13,10 +15,18 @@ def git_pull():
         logger.error(e)
 
 
+def parse():
+    parser = ArgumentParser()
+    parser.add_argument("--mode", default="prod", choices=["prod", "test"])
+    parser.add_argument("--nonblock", default=True, action="store_false")
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
     date_id = datetime.now().strftime("%Y-%m-%d")
-    mode = "prod"
-    block = False if mode == "test" else True
+    args = parse()
+    mode = args.mode.lower()
+    block = args.nonblock
 
     bm = BatchManager(task_id=get_task_id(__file__), key=date_id, block=block)
     bm(task_type="execute", func=git_pull)
--- src/git_push.py
+++ src/git_push.py
@@ -1,5 +1,6 @@
 from utils import PathConfig
 from utils import BatchManager, get_task_id
+from argparse import ArgumentParser
 from git import Repo
 from loguru import logger
 from datetime import datetime
@@ -16,10 +17,18 @@ def git_push(date_id: str):
         logger.error(e)
 
 
+def parse():
+    parser = ArgumentParser()
+    parser.add_argument("--mode", default="prod", choices=["prod", "test"])
+    parser.add_argument("--nonblock", default=True, action="store_false")
+    return parser.parse_args()
+
+
 if __name__ == "__main__":
     date_id = datetime.now().strftime("%Y-%m-%d")
-    mode = "prod"
-    block = False if mode == "test" else True
+    args = parse()
+    mode = args.mode.lower()
+    block = args.nonblock
 
     bm = BatchManager(task_id=get_task_id(__file__), key=date_id, block=block)
     bm(task_type="execute", func=git_push, date_id=date_id)
